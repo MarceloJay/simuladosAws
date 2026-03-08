6cc9ee6aaa59e807ae5f8a6fee0c62bcd9327e71
MarceloJay <jaytilangus@gmail.com>
2026-03-07 23:13:29 -0300

    Bump version to 1.2.2 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jaydev.awsquiz"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.2.1"
+        versionCode = 10
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
